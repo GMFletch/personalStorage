e5cfda1ab13940fafa7835490ef55f86fa2fbd1b
Levi Fletcher <mail.levifletcher@gmail.com>
2022-05-05 16:39:04 -0700

    finshed part 3

--- datavis-with-curran/Make a face/Make a face - part 3.js
+++ datavis-with-curran/Make a face/Make a face - part 3.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { arc } from 'd3';
 
 const width = 960;
 const height = 500;
@@ -7,30 +8,31 @@ const centerX = width / 2;
 const centerY = height / 2;
 const strokeWidth = 10;
 const eyeOffsetX = 90;
-const eyeOffsetY = 90;
-const eyeRadius = 50;
+const eyeOffsetY = 100;
+const eyeRadius = 40;
+const mouthWidth = 20;
+const mouthRadius = 140;
+
+const mouthArc = arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth)
+  .startAngle(Math.PI / 2)
+  .endAngle((Math.PI * 3) / 2);
 
 const App = () => {
   return (
     <svg width={width} height={height}>
-      <circle
-        cx={centerX}
-        cy={centerY}
-        r={centerY - strokeWidth}
-        fill="yellow"
-        stroke="black"
-        stroke-width={strokeWidth}
-      />
-      <circle
-        cx={centerX - eyeOffsetX}
-        cy={centerY - eyeOffsetY}
-        r={eyeRadius}
-      />
-      <circle
-        cx={centerX + eyeOffsetX}
-        cy={centerY - eyeOffsetY}
-        r={eyeRadius}
-      />
+      <g transform={`translate(${centerX},${centerY})`}>
+        <circle
+          r={centerY - strokeWidth}
+          fill="yellow"
+          stroke="black"
+          stroke-width={strokeWidth}
+        />
+        <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+        <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+        <path d={mouthArc()} />
+      </g>
     </svg>
   );
 };
