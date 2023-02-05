aeff3f917e889a16105e978a5d4007f77810e26d
levifletcher <mail.levifletcher@gmail.com>
2023-02-04 21:50:19 -0800

    another

--- CodeSignal/intro-45-buildPalindrome.js
+++ CodeSignal/intro-45-buildPalindrome.js
@@ -1,1 +1,16 @@
-function solution(st) {}
+function solution(st) {
+  return recursor(st);
+  function recursor(string, num = 0) {
+    const tempStringArray = string.split('');
+    for (let i = 0; i < num; i++) {
+      tempStringArray.push(string[num - 1 - i]);
+    }
+    const tempString = tempStringArray.join('');
+    if (tempString === tempString.split('').reverse().join('')) {
+      return tempString;
+    } else {
+      num++;
+      return recursor(string, num);
+    }
+  }
+}
