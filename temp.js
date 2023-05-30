0b69ed531400bb93f1567147523df73817be7491
levifletcher <mail.levifletcher@gmail.com>
2023-05-30 08:55:37 -0700

    save

--- temp.js
+++ temp.js
@@ -1,27 +1,8 @@
-const pointObject = updateSelectedPoint();
+const myArray = [1, 2, , 4];
 
-function updateSelectedPoint() {
-    const pointArray = ["A", "B", "H", "I", "J", "K", "N", "O"];
-    const segArray = [
-        ["a", "b"],
-        ["c", "d"],
-        ["e", "o"],
-        ["t", "u"]
-    ];
-    const tempObj = {};
-    pointArray.forEach(function (el, index) {
-        const tempFloor = Math.floor(index / 2);
-        tempObj[el] = el;
-        tempObj[el].point = ggbObject.getValue(segArray[tempFloor][index % 2]);
-        tempObj[el].attachedSeg = ggbObject.getValue(
-            segArray[tempFloor][(index + 1) % 2]
-        );
-        tempObj[el].otherSeg = ggbObject.getValue("b");
-        tempObj[el].slopeTri = ggbObject.getValue("b");
-        tempObj[el].otherPoint = ggbObject.getValue("b");
-        tempObj[el].rise = ggbObject.getValue("b");
-        tempObj[el].run = ggbObject.getValue("b");
-    });
+const return1 = myArray.map((x) => 2 * x);
 
-    return tempObj;
-}
+const return2 = Array.apply(null, { length: 9 });
+
+console.log(return1);
+console.log(return2);
