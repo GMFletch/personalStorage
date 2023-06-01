3e0804fd81a2e360958ad78f16a1e4df2ff334e2
levifletcher <mail.levifletcher@gmail.com>
2023-06-01 10:20:33 -0700

    update

--- temp.js
+++ temp.js
@@ -1,8 +1,18 @@
-const myArray = [1, 2, , 4];
+function ggbOnInit() {
+    //console.log(ggbApplet.getXML("B"));
+    ggbApplet.registerObjectUpdateListener("B", function () {
+        const param = ggbApplet.getValue("PathParameter(B)");
+        if (param > 0.75 || param < 0.25) {
+            const newIndex =
+                (ggbApplet.getValue("circlePathIncrement") *
+                    ggbApplet.getValue("numRotations")) /
+                    2 +
+                ggbApplet.getValue("indexB");
+            console.log(newIndex);
 
-const return1 = myArray.map((x) => 2 * x);
-
-const return2 = Array.apply(null, { length: 9 });
-
-console.log(return1);
-console.log(return2);
+            ggbApplet.evalCommand(
+                "SetValue(B, BPath(".concat(newIndex.toString(), "))")
+            );
+        }
+    });
+}
