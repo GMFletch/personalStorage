c723e4cd6caa5d6d383e466c10f3f4474186720e
levifletcher <levi.fletcher@greatminds.org>
2023-11-08 15:30:21 -0800

    updates day 2 challenge

---
+++ src/Advent of Code/2022/day-1.ts
@@ -0,0 +1,267 @@
+import inputData from "./inputAsJS-1.js";
+
+const round1: string[] = inputData
+  .replaceAll(`\n`, "x")
+  .replaceAll(`;`, "")
+  .split("xx");
+
+const round2 = round1.reduce(
+  (arrayOfArrays, currentElf, index) => {
+    const totalCalsForCurrentElf = currentElf
+      .split("x")
+      .reduce((totalCarried: number, currentItem: string) => {
+        totalCarried + parseInt(currentItem);
+        return totalCarried + parseInt(currentItem);
+      }, 0);
+    const spliceIndex = arrayOfArrays.findIndex((subArray) => {
+      return subArray[0] < totalCalsForCurrentElf;
+    });
+    if (spliceIndex !== -1) {
+      arrayOfArrays.splice(spliceIndex, 0, [totalCalsForCurrentElf, index]);
+      arrayOfArrays.pop();
+    }
+    return arrayOfArrays;
+  },
+  [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+  ]
+);
+
+console.log(round2);
+console.log(round2[0][0] + round2[1][0] + round2[2][0]);
+
+// import inputData from "./textAsJS.js";
+
+// const round1: string[] = inputData
+//   .replaceAll(`\n`, "x")
+//   .replaceAll(`;`, "")
+//   .split("xx");
+
+// const round2 = round1.reduce(
+//   (
+//     overall: {
+//       highestIndex: number;
+//       highestCals: number;
+//       secondIndex: number;
+//       secondCals: number;
+//       thirdIndex: number;
+//       thirdCals: number;
+//     },
+//     currentElf,
+//     index
+//   ) => {
+//     const totalCalsForCurrentElf = currentElf
+//       .split("x")
+//       .reduce((totalCarried: number, currentItem: string) => {
+//         totalCarried + parseInt(currentItem);
+//         return totalCarried + parseInt(currentItem);
+//       }, 0);
+//     switch (true) {
+//       case totalCalsForCurrentElf > overall.highestCals: {
+//         // set as new highest, and shift everything down
+//         console.log("highest");
+//         console.log("index: %o, cals: %o", index, totalCalsForCurrentElf);
+//         console.log(
+//           "highestIndex: %o, highestCals: %o",
+//           overall.highestIndex,
+//           overall.highestCals
+//         );
+//         const newSecond = setHighest(totalCalsForCurrentElf, index);
+//         const newThird = setSecond(
+//           newSecond.removedNum,
+//           newSecond.removedIndex
+//         );
+//         setThird(newThird.removedNum, newThird.removedIndex);
+//         break;
+//       }
+//       case totalCalsForCurrentElf > overall.secondCals: {
+//         // set as new second, and shift everything down
+//         console.log("second");
+//         console.log("index: %o, cals: %o", index, totalCalsForCurrentElf);
+//         console.log(
+//           "highestIndex: %o, highestCals: %o",
+//           overall.highestIndex,
+//           overall.highestCals
+//         );
+//         const newThird = setSecond(totalCalsForCurrentElf, index);
+//         setThird(newThird.removedNum, newThird.removedIndex);
+//         break;
+//       }
+//       case totalCalsForCurrentElf > overall.thirdCals: {
+//         // set as new third
+//         console.log("third");
+//         console.log("index: %o, cals: %o", index, totalCalsForCurrentElf);
+//         console.log(
+//           "highestIndex: %o, highestCals: %o",
+//           overall.highestIndex,
+//           overall.highestCals
+//         );
+//         setThird(totalCalsForCurrentElf, index);
+//         break;
+//       }
+//       default: {
+//         // do nothing!
+//         break;
+//       }
+//     }
+//     function setHighest(
+//       number: number,
+//       index: number
+//     ): { removedNum: number; removedIndex: number } {
+//       const returnObj = {
+//         removedNum: overall.highestCals,
+//         removedIndex: overall.highestIndex,
+//       };
+//       overall.highestCals = number;
+//       overall.highestIndex = index;
+//       return returnObj;
+//     }
+//     function setSecond(
+//       number: number,
+//       index: number
+//     ): { removedNum: number; removedIndex: number } {
+//       const returnObj = {
+//         removedNum: overall.secondCals,
+//         removedIndex: overall.secondIndex,
+//       };
+//       overall.secondCals = number;
+//       overall.secondIndex = index;
+//       return returnObj;
+//     }
+//     function setThird(number: number, index: number): void {
+//       overall.thirdCals = number;
+//       overall.thirdIndex = index;
+//     }
+//     return overall;
+//   },
+//   {
+//     highestIndex: 0,
+//     highestCals: 0,
+//     secondIndex: 0,
+//     secondCals: 0,
+//     thirdIndex: 0,
+//     thirdCals: 0,
+//   }
+// );
+
+// console.log(round2);
+// console.log(round2.highestCals + round2.secondCals + round2.thirdCals);
+
+// import inputData from "./textAsJS.js";
+
+// const round1: string[] = inputData
+//   .replaceAll(`\n`, "x")
+//   .replaceAll(`;`, "")
+//   .split("xx");
+
+// const round2 = round1.reduce(
+//   (
+//     overall: {
+//       highestIndex: number;
+//       highestCals: number;
+//       secondIndex: number;
+//       secondCals: number;
+//       thirdIndex: number;
+//       thirdCals: number;
+//     },
+//     currentElf,
+//     index
+//   ) => {
+//     const totalCalsForCurrentElf = currentElf
+//       .split("x")
+//       .reduce((totalCarried: number, currentItem: string) => {
+//         totalCarried + parseInt(currentItem);
+//         return totalCarried + parseInt(currentItem);
+//       }, 0);
+//     switch (true) {
+//       case totalCalsForCurrentElf > overall.highestCals: {
+//         // set as new highest, and shift everything down
+//         console.log("highest");
+//         console.log("index: %o, cals: %o", index, totalCalsForCurrentElf);
+//         console.log(
+//           "highestIndex: %o, highestCals: %o",
+//           overall.highestIndex,
+//           overall.highestCals
+//         );
+//         const newSecond = setHighest(totalCalsForCurrentElf, index);
+//         const newThird = setSecond(
+//           newSecond.removedNum,
+//           newSecond.removedIndex
+//         );
+//         setThird(newThird.removedNum, newThird.removedIndex);
+//         break;
+//       }
+//       case totalCalsForCurrentElf > overall.secondCals: {
+//         // set as new second, and shift everything down
+//         console.log("second");
+//         console.log("index: %o, cals: %o", index, totalCalsForCurrentElf);
+//         console.log(
+//           "highestIndex: %o, highestCals: %o",
+//           overall.highestIndex,
+//           overall.highestCals
+//         );
+//         const newThird = setSecond(totalCalsForCurrentElf, index);
+//         setThird(newThird.removedNum, newThird.removedIndex);
+//         break;
+//       }
+//       case totalCalsForCurrentElf > overall.thirdCals: {
+//         // set as new third
+//         console.log("third");
+//         console.log("index: %o, cals: %o", index, totalCalsForCurrentElf);
+//         console.log(
+//           "highestIndex: %o, highestCals: %o",
+//           overall.highestIndex,
+//           overall.highestCals
+//         );
+//         setThird(totalCalsForCurrentElf, index);
+//         break;
+//       }
+//       default: {
+//         // do nothing!
+//         break;
+//       }
+//     }
+//     function setHighest(
+//       number: number,
+//       index: number
+//     ): { removedNum: number; removedIndex: number } {
+//       const returnObj = {
+//         removedNum: overall.highestCals,
+//         removedIndex: overall.highestIndex,
+//       };
+//       overall.highestCals = number;
+//       overall.highestIndex = index;
+//       return returnObj;
+//     }
+//     function setSecond(
+//       number: number,
+//       index: number
+//     ): { removedNum: number; removedIndex: number } {
+//       const returnObj = {
+//         removedNum: overall.secondCals,
+//         removedIndex: overall.secondIndex,
+//       };
+//       overall.secondCals = number;
+//       overall.secondIndex = index;
+//       return returnObj;
+//     }
+//     function setThird(number: number, index: number): void {
+//       overall.thirdCals = number;
+//       overall.thirdIndex = index;
+//     }
+//     return overall;
+//   },
+//   {
+//     highestIndex: 0,
+//     highestCals: 0,
+//     secondIndex: 0,
+//     secondCals: 0,
+//     thirdIndex: 0,
+//     thirdCals: 0,
+//   }
+// );
+
+// console.log(round2);
+// console.log(round2.highestCals + round2.secondCals + round2.thirdCals);
---
+++ src/Advent of Code/2022/day-2-2.ts
@@ -0,0 +1,213 @@
+// attempt 1: [Done] exited with code=0 in 0.306 seconds
+import inputData from "./inputAsJS-2-1.js";
+
+console.log(typeof inputData);
+
+const round1: string[] = inputData.replaceAll(` `, "").split(`\n`);
+
+console.log(round1);
+
+const round2 = round1.reduce((currentTotal, currentString) => {
+  let scoreThisRound = 0;
+  const rock = 1;
+  const paper = 2;
+  const scissors = 3;
+  const lose = 0;
+  const draw = 3;
+  const win = 6;
+  switch (currentString[0]) {
+    case "A": {
+      // opponent chose rock
+      switch (currentString[1]) {
+        case "X": {
+          // you need to lose, so you choose scissors
+          scoreThisRound = lose + scissors;
+          break;
+        }
+        case "Y": {
+          // you need to draw, so you choose rock
+          scoreThisRound = draw + rock;
+          break;
+        }
+        case "Z": {
+          // you need to win, so you choose paper
+          scoreThisRound = win + paper;
+          break;
+        }
+        default: {
+          // error if getting here
+          console.warn("error in A switch statement");
+          break;
+        }
+      }
+      break;
+    }
+    case "B": {
+      // opponent chose paper
+      switch (currentString[1]) {
+        case "X": {
+          // you need to lose, so you choose rock
+          scoreThisRound = lose + rock;
+          break;
+        }
+        case "Y": {
+          // you need to draw, so you choose paper
+          scoreThisRound = draw + paper;
+          break;
+        }
+        case "Z": {
+          // you need to win, so you choose scissors
+          scoreThisRound = win + scissors;
+          break;
+        }
+        default: {
+          // error if getting here
+          console.warn("error in B switch statement");
+          break;
+        }
+      }
+      break;
+    }
+    case "C": {
+      // opponent chose scissors
+      switch (currentString[1]) {
+        case "X": {
+          // you need to lose, so you choose paper
+          scoreThisRound = lose + paper;
+          break;
+        }
+        case "Y": {
+          // you need to draw, so you choose scissors
+          scoreThisRound = draw + scissors;
+          break;
+        }
+        case "Z": {
+          // you need to win, so you choose rock
+          scoreThisRound = win + rock;
+          break;
+        }
+        default: {
+          // error if getting here
+          console.warn("error in C switch statement");
+          break;
+        }
+      }
+      break;
+    }
+    default: {
+      // error if getting here
+      console.warn("error in currentString switch statement");
+      break;
+    }
+  }
+  return currentTotal + scoreThisRound;
+}, 0);
+
+console.log(round2);
+
+// // attempt 1: [Done] exited with code=0 in 0.306 seconds
+// import inputData from "./inputAsJS-2-1.js";
+
+// console.log(typeof inputData);
+
+// const round1: string[] = inputData.replaceAll(` `, "").split(`\n`);
+
+// console.log(round1);
+
+// const round2 = round1.reduce((currentTotal, currentString) => {
+//   let scoreThisRound = 0;
+//   const rock = 1;
+//   const paper = 2;
+//   const scissors = 3;
+//   const lose = 0;
+//   const draw = 3;
+//   const win = 6;
+//   switch (currentString[0]) {
+//     case "A": {
+//       // opponent chose rock
+//       switch (currentString[1]) {
+//         case "X": {
+//           // you need to lose, so you choose scissors
+//           scoreThisRound = lose + scissors;
+//           break;
+//         }
+//         case "Y": {
+//           // you need to draw, so you choose rock
+//           scoreThisRound = draw + rock;
+//           break;
+//         }
+//         case "Z": {
+//           // you need to win, so you choose paper
+//           scoreThisRound = win + paper;
+//           break;
+//         }
+//         default: {
+//           // error if getting here
+//           console.warn("error in A switch statement");
+//           break;
+//         }
+//       }
+//       break;
+//     }
+//     case "B": {
+//       // opponent chose paper
+//       switch (currentString[1]) {
+//         case "X": {
+//           // you need to lose, so you choose rock
+//           scoreThisRound = lose + rock;
+//           break;
+//         }
+//         case "Y": {
+//           // you need to draw, so you choose paper
+//           scoreThisRound = draw + paper;
+//           break;
+//         }
+//         case "Z": {
+//           // you need to win, so you choose scissors
+//           scoreThisRound = win + scissors;
+//           break;
+//         }
+//         default: {
+//           // error if getting here
+//           console.warn("error in B switch statement");
+//           break;
+//         }
+//       }
+//       break;
+//     }
+//     case "C": {
+//       // opponent chose scissors
+//       switch (currentString[1]) {
+//         case "X": {
+//           // you need to lose, so you choose paper
+//           scoreThisRound = lose + paper;
+//           break;
+//         }
+//         case "Y": {
+//           // you need to draw, so you choose scissors
+//           scoreThisRound = draw + scissors;
+//           break;
+//         }
+//         case "Z": {
+//           // you need to win, so you choose rock
+//           scoreThisRound = win + rock;
+//           break;
+//         }
+//         default: {
+//           // error if getting here
+//           console.warn("error in C switch statement");
+//           break;
+//         }
+//       }
+//       break;
+//     }
+//     default: {
+//       // error if getting here
+//       console.warn("error in currentString switch statement");
+//       break;
+//     }
+//   }
+//   return currentTotal + scoreThisRound;
+// }, 0);
+
+// console.log(round2);
